a7ec012ca6c95bd1ee75f87d7caf9e938cbe4907
Victo <Victo@LAPTOP-FQ1F9URR>
2020-06-23 16:09:53 -0300

    Configurando a conexão com o banco de dados.

---
+++ QuickBuy.Repositorio/Contexto/QuickBuy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Repositorio.Repositorios;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+    public class QuickBuyContexto : DbContext 
+    {
+      
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Produto> Produtos  { get; set; }
+        public DbSet<Pedido>  Pedidos { get; set; }
+        public DbSet<ItemPedido> ItensPedidos { get; set; }
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+        public QuickBuyContexto([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ QuickBuy.Repositorio/Contexto/Usuario.cs
@@ -0,0 +1,6 @@
+﻿namespace QuickBuy.Repositorio.Contexto
+{
+    internal class Usuario
+    {
+    }
+}
